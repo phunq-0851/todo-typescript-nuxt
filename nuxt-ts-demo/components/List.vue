9976d3b77d44ed85fec556f6ea0e783edf63bdde
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2021-06-07 13:29:38 +0700

    update

--- nuxt-ts-demo/components/List.vue
+++ nuxt-ts-demo/components/List.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
   <el-table
-    :data="listData"
+    :data="cloneData"
     style="width: 100%">
     <el-table-column
       label="Name"
       width="200">
       <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
+        <el-popover v-if="edit !== scope.$index" trigger="hover" placement="top">
           <p>Name: {{ scope.row.name }}</p>
           <div slot="reference" class="name-wrapper">
             <el-tag size="medium">{{ scope.row.name }}</el-tag>
           </div>
         </el-popover>
+        <div v-else>
+          <el-input v-model="scope.row.name"></el-input>
+        </div>
       </template>
     </el-table-column>
     <el-table-column
       label="Operations"
       width="200">
       <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        <div v-if="edit !== scope.$index">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index)">Edit</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index)">Delete</el-button>
+        </div>
+        <div v-else>
+          <el-button
+            size="mini"
+            @click="save(scope.$index, scope.row)">Save</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="save(scope.$index, scope.row)">Cancel</el-button>
+        </div>
       </template>
     </el-table-column>
   </el-table>
@@ -35,15 +49,26 @@
 <script>
 import {Component, Vue, Prop} from 'nuxt-property-decorator'
 @Component
-export default class MyCom extends Vue {
+export default class MyList extends Vue {
   @Prop({ type: Array, required: true, default: () => { return [] } }) listData
 
-  handleEdit(index, row) {
-    console.log(index, row);
+  cloneData = []
+  edit = -1
+
+  handleEdit(index) {
+    this.edit = index
   }
 
-  handleDelete(index, row) {
-    console.log(index, row);
+  handleDelete(index) {
+    this.cloneData.splice(index, 1)
   }
+
+  save (index, row) {
+    this.edit = -1
   }
+
+  mounted () {
+    this.cloneData = this.listData
+  }
+}
 </script>
